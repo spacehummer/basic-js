52834a19a912c9a041ea3469e277ba4e1ead2eb4
Yuriy Lavrushin <yuriy.lavroushin@gmail.com>
2023-04-16 21:16:25 +0400

    feat: solution for `Carbon dating` is ready.

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -21,17 +21,34 @@ function dateSample(sampleActivity) {
   // throw new NotImplementedError('Not implemented');
   // // remove line with error and write your code here
 
-  console.log("---- Source data:", sampleActivity);
+  let t = null;
 
-  console.log("---- Calculated age:", null);
+  console.log("---- Source data:", sampleActivity);
+  console.log("---- Rounded data:", Math.ceil(Number(sampleActivity)));
+
+  if (
+    typeof (Number(sampleActivity)) !== "number"
+    || Number(sampleActivity) <= 0
+    || Number(sampleActivity) >= 15
+    || typeof sampleActivity !== "string"
+    || isNaN(Number(sampleActivity))
+  ) {
+    console.log("---- Source data is in wrong type!");
+    return false;
+  }
+
+  t = Math.ceil((Math.log(MODERN_ACTIVITY/(Number(sampleActivity)))*HALF_LIFE_PERIOD)/Math.log(2));
+
+  console.log("---- Calculated age:", t);
+  return t;
 
 }
 
 // Test scripts
 // npm run test > "./logs/git_log_all_tests_-_$(date +%Y-%m-%d_-_%k_%M_%S).log"
-// npm run test ./test/count-cats.test.js  > "./logs/git_log_count_cats_-_$(date +%Y-%m-%d_-_%k_%M_%S).log"
+// npm run test ./test/carbon-dating.test.js  > "./logs/git_log_count_cats_-_$(date +%Y-%m-%d_-_%k_%M_%S).log"
 
-dateSample();
+dateSample("ACTIVITY OVER 9000");
 
 module.exports = {
   dateSample
